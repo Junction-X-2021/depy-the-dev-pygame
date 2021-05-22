1be5c9178c326986fb5ceb5de609906f581092c0
g4dra <g4dra@noreply.github.com>
2021-05-23 06:25:11 +0900

    player class 수정
    player class가 여러 용도로 쓰일 수 있게 수정

--- player.py
+++ player.py
@@ -4,21 +4,40 @@ import pygame.image
 
 
 class Player:
-    def __init__(self, x, y):
+    def __init__(self, x, y, player_type):
         self.x = x
         self.y = y
         self.count = 0
-        self.player_image = pygame.image.load(f"assets/Idle{random.randint(1, 2)}.png")
+        self.player_type = player_type
+        if player_type == "developer":
+            self.player_image = pygame.image.load(f"assets/Idle{random.randint(1, 2)}.png")
+        elif player_type == "ceo":
+            self.player_image = pygame.image.load(f"assets/cIdle{random.randint(1, 2)}.png")
+        else:
+            self.player_image = pygame.image.load(f"assets/eIdle{random.randint(1, 2)}.png")
+
         self.player_image = pygame.transform.scale(self.player_image, (64, 64))
 
     def move_to_right(self):
-        self.player_image = pygame.image.load(f"assets/Walk{random.randint(1, 5)}.png")
+        global player_type
+
+        if self.player_type == "player":
+            self.player_image = pygame.image.load(f"assets/Walk{random.randint(1, 5)}.png")
+        elif self.player_type == "ceo":
+            self.player_image = pygame.image.load(f"assets/cWalk{random.randint(1, 5)}.png")
+        else:
+            self.player_image = pygame.image.load(f"assets/eWalk{random.randint(1, 5)}.png")
         self.player_image = pygame.transform.scale(self.player_image, (64, 64))
         self.x += 1
         self.count += 1
 
     def move_to_left(self):
-        self.player_image = pygame.image.load(f"assets/Walk{random.randint(1, 5)}.png")
+        if self.player_type == "player":
+            self.player_image = pygame.image.load(f"assets/Walk{random.randint(1, 5)}.png")
+        elif self.player_type == "ceo":
+            self.player_image = pygame.image.load(f"assets/cWalk{random.randint(1, 5)}.png")
+        else:
+            self.player_image = pygame.image.load(f"assets/eWalk{random.randint(1, 5)}.png")
         self.player_image = pygame.transform.flip(self.player_image, True, False)
         self.player_image = pygame.transform.scale(self.player_image, (64, 64))
         self.x -= 1
